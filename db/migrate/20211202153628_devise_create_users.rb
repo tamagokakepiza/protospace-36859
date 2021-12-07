76a892605063c009b0c7c2fc99e7a8e99613abfa
tamagokakepiza <nyororo89@gmail.com>
2021-12-07 19:27:57 +0900

    投稿機能の実装

--- db/migrate/20211202153628_devise_create_users.rb
+++ db/migrate/20211202153628_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name,       null: false, default: ""
-      t.text   :profile,    null: false
-      t.text   :occupation, null: false
-      t.text   :position,   null: false
+      t.text   :profile,    null: false, default: ""
+      t.text   :occupation, null: false, default: ""
+      t.text   :position,   null: false, default: ""
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
